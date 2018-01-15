e2ad960b31adfd8fac67d8be48762b551abc6970
phirmware <chibuzor.ojukwu@gmail.com>
2018-01-15 22:34:23 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# This is a fun movie search engine
+## Download the zip into a folder you created
+## Enter into folder at your terminal
+## Run  : *npm install request ejs express*
+#After installing, Run:
+## *node app.js*
---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var request = require('request');
+var app = express();
+app.set('view engine', 'ejs');
+
+
+
+app.get('/',function(req,res){
+    res.render('home');
+})
+
+app.get('/result',function(req,res){
+    var input = req.query.info;
+    var url = 'http://api.tvmaze.com/search/shows?q=' + input;
+    request(url,function(error,response,body){
+        var data = JSON.parse(body);
+        res.render('index',{data:data});
+    })
+})
+
+
+app.listen(3000,function(){
+    console.log('listening at port 3000');
+})
