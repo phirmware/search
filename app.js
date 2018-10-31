4266c5f44430baf76b1ce6325c4225ed033e51cd
phirmware <32414323+phirmware@users.noreply.github.com>
2018-10-31 12:26:53 +0000

    update

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.get('/result',function(req,res){
 });
 
 
-app.listen(3000,function(){
-    console.log('listening at port 3000');
+app.listen(3001,function(){
+    console.log('listening at port 3001');
 });
